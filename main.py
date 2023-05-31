d3094005aaed7d75df885779596d1626b5e064bb
Lev Tokmakov <82312679+Zemanqwerty@users.noreply.github.com>
2023-05-31 15:30:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,26 @@ tello.streamon()
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
+def check_battery():
+    current_battery = tello.get_battery()
+    string_battery = ''
+    battery_color = (0, 0, 0)
+
+    for i in range(current_battery // 10):
+        string_battery += '-'
+
+    if current_battery > 75:
+        battery_color = (35, 210, 22)
+    elif current_battery > 50:
+        battery_color = (47, 225, 207)
+    elif current_battery > 25:
+        battery_color = (0, 128, 255)
+    else:
+        battery_color = (0, 0, 255)
+
+    return battery_color, string_battery
+
+
 def fly(x, y, w, h):
     x_move = 0
     y_move = 0
@@ -62,11 +82,17 @@ while True:
     else:
         tello.send_rc_control(0, 0, 0, 0)
 
-    cv2.putText(r_frame, f'BATTERY: {tello.get_battery()}%', (10, 470), cv2.FONT_HERSHEY_SIMPLEX, 1, (180, 230, 100), 4)
+    b_c, b_v = check_battery()
+
+    cv2.putText(r_frame, f'BATTERY:', (10, 470), cv2.FONT_HERSHEY_SIMPLEX, 1, (180, 230, 100), 4)
+    cv2.putText(r_frame, f'{b_v}', (160, 475), cv2.FONT_HERSHEY_SIMPLEX, 1, b_c, 4)
+    cv2.putText(r_frame, f'{b_v}', (160, 465), cv2.FONT_HERSHEY_SIMPLEX, 1, b_c, 4)
     cv2.putText(r_frame, f'HEIGHT: {tello.get_height()}sm', (10, 420), cv2.FONT_HERSHEY_SIMPLEX, 1, (180, 230, 100), 4)
-    cv2.putText(r_frame, f'TOTAL TIME: {tello.get_battery()}s', (10, 370), cv2.FONT_HERSHEY_SIMPLEX, 1, (180, 230, 100), 4)
+    cv2.putText(r_frame, f'TOTAL TIME: {tello.get_flight_time()}s', (10, 370), cv2.FONT_HERSHEY_SIMPLEX, 1, (180, 230, 100),
+                4)
     cv2.imshow('TELLO AI', r_frame)
-        
+
+
     if cv2.waitKey(1) == ord('q'):
         break
 
